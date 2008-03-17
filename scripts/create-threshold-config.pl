8ba65d4ec1756aef86c896c99eeac8717d5b6c6e
olesenm <olesenm@03cb2d7b-d512-0410-b4a7-c1606bda5a11>
2008-03-17 23:28:52 +0000

    modified to optionally create unqualified hosts
    git-svn-id: svn+ssh://bioteam/home/svn-repository/xml-qstat/trunk@23 03cb2d7b-d512-0410-b4a7-c1606bda5a11

--- scripts/create-threshold-config.pl
+++ scripts/create-threshold-config.pl
@@ -1,61 +1,67 @@
 #!/usr/bin/perl -w
+use strict;
 
-#-----------------------
-# Usage: 
+# -----------------------
+# Usage:
 #
 #   Make sure you can run "qstat -f" (SGE env is sourced)
 #   Then call this script, it will spit XML to STDOUT which
 #   can be directed into a config file. If it looks like it
 #   is producing good output then follow the example below
 #   to create the configuration file.
-# #
-# Example:
 #
+# Example:
 #  ./create-threshold-config.pl > ../xmlqstat/xml/CONFIG_alarm-threshold.xml
 #
 # Once CONFIG_alarm-threshold.xml exists, edit CONFIG.xml to
 # enable this feature in the web interface.
 #
-#-----------------------
+# -----------------------
 
-my @response      = `qstat -f -xml | grep '<name>'`;
-my $queueInstance = undef;
-my $qconfResponse = undef;
-my $threshold = undef;
+# remove domains?
+my $unqualified;
 
-print STDOUT <<EOF;
+
+print <<"PRINT";
 <!-- XML-QSTAT load_alarm_threshold configuration file -->
-<!-- This file caches site-specific SGE queue load 
-     alarm thresholds so that the xmlqstat interface
-     can compare the set alarm value with the dynamically
-     reported load averages. This allows us to take action
-     or at least render the HTML differently if a load average
-     is close to exceeding the load alarm threshold.
--->
+<!--
+   | This file caches site-specific SGE queue load alarm thresholds so
+   | that the xmlqstat interface can compare the set alarm value with
+   | the dynamically reported load averages.
+   | This allows us to take action or at least render the HTML differently
+   | if a load average is close to exceeding the load alarm threshold.
+   -->
 <config>
 <load_alarm_threshold>
-EOF
+PRINT
 
 ## Now do some work ...
+@ARGV = "qstat -xml -f |";
 
-foreach(@response) {
+while (<>) {
     ## pull out the full name of the queue instance
-    if(/<name>(.*)<\/name>/) { $queueInstance = $1; }
+    s{^\s*<name>\s*|\s*</name>\s*$}{}g or next;
 
-    ## Call "qconf -sq <queue name>" to learn the configured threshold
-    $qconfResponse = `qconf -sq $queueInstance | grep load_threshold`;
+    if ($unqualified) {
+        s{(\@[^.]+)\..+$}{$1};
+    }
 
-    ## pull out the value of np_load_avg from the qconf -sq response
-    if($qconfResponse =~ (/np_load_avg=(.*)$/)) { $threshold=$1; }
+    my $qInstance = $_;
 
-    print STDOUT "<qi name=\"$queueInstance\" ";
-    print STDOUT "np_load_alarm=\"$threshold\" \/>\n";
+    ## get configured threshold via "qconf -sq <queueName>"
+    my ($threshold) =
+      map { s/^load_thresholds\s+np_load_avg=// ? $_ : () }
+      qx{qconf -sq $qInstance};
 
+    chomp( $threshold ||= 0 );
+
+    print qq{  <qi name="$qInstance" np_load_alarm="$threshold" />\n};
 }
 
-
-print STDOUT <<EOF;
+print <<"PRINT";
 </load_alarm_threshold>
 </config>
 
-EOF
+PRINT
+
+## ---------------------------------------------------------------- end-of-file
