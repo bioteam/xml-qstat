0f8d59becc4c955211706056128115e92ec0af04
Mark Olesen <Mark.Olesen@emconTechnologies.com>
2009-06-17 10:16:44 +0200

    harden wrapper scripts (qhost.sh, qstat.sh)
    - avoid eval
    - handle invalid SGE_ROOT and SGE_CELL etc
    - emit xml error messages
    - integrate qhost workaround into wrapper too

--- scripts/qhost.sh
+++ scripts/qhost.sh
@@ -6,19 +6,33 @@
 #
 # But the wrapper also interprets these initial parameters:
 #     SGE_CELL
-#     SGE_ROOT
-# ----------------------------------------------------------------------
+#     SGE_ROOT (should be an absolute path)
+#
+# -----------------------------------------------------------------------------
+
+#
+# NB: using CDATA in the error messages doesn't seem to help with bad characters
+#
+error()
+{
+    echo "<?xml version='1.0'?><error>"
+    while [ "$#" -ge 1 ]; do echo "$1"; shift; done
+    echo "</error>"
+    exit 1
+}
+
+
 # find initial SGE_* parameters
 unset settings
 while [ "$#" -gt 0 ]
 do
     case "$1" in
     SGE_CELL=*)
-        eval "export ${1%%/}"
+        export SGE_CELL="${1##SGE_CELL=}"
         shift
         ;;
     SGE_ROOT=*)
-        eval "export ${1%%/}"
+        export SGE_ROOT="${1##SGE_ROOT=}"
         settings=true
         shift
         ;;
@@ -29,18 +43,29 @@ do
 done
 
 
+# require a good SGE_ROOT and an absolute path:
+[ -d "$SGE_ROOT" -a "${SGE_ROOT##/}" != "$SGE_ROOT" ] || \
+    error "invalid SGE_ROOT directory '$SGE_ROOT'"
+
+# require a good SGE_CELL:
+[ -d "$SGE_ROOT/${SGE_CELL:-default}" ] || \
+    error "invalid SGE_CELL directory '$SGE_ROOT/${SGE_CELL:-default}'"
+
+
+# this is the essential bit from settings.sh,
+# but SGE_ROOT might be different
 if [ ${settings:-false} = true ]
 then
-    # this is the essential bit from settings.sh,
-    # but SGE_ROOT might be different
-    if [ -x $SGE_ROOT/util/arch ]
+    if [ -x "$SGE_ROOT/util/arch" ]
     then
-         PATH=$SGE_ROOT/bin/`$SGE_ROOT/util/arch`:$PATH
-         export PATH
+        PATH=$SGE_ROOT/bin/$($SGE_ROOT/util/arch):$PATH
+        export PATH
+    else
+        error "'$SGE_ROOT/util/arch' not found"
     fi
 fi
 
 
-qhost "$@" | sed -e 's/xmlns=/xmlns:xsd=/'
+qhost "$@" | sed -e 's@xmlns=@xmlns:xsd=@'
 
 # ----------------------------------------------------------------- end-of-file
--- scripts/qstat.sh
+++ scripts/qstat.sh
@@ -4,8 +4,16 @@
 #
 # But the wrapper also interprets these initial parameters:
 #     SGE_CELL
-#     SGE_ROOT
-# ----------------------------------------------------------------------
+#     SGE_ROOT (should be an absolute path)
+#
+# and also supplies a quick fix for issue
+#     http://gridengine.sunsource.net/issues/show_bug.cgi?id=2515
+# for older installations (don't rely on the exit code)
+#
+# as well as a quick fix for issue
+#     http://gridengine.sunsource.net/issues/show_bug.cgi?id=3057
+#
+# -----------------------------------------------------------------------------
 
 # The command is basename w/o trailing .sh
 cmd=${0##*/}
@@ -22,17 +30,29 @@ fi
 
 echo "$(date --rfc-3339=s) $USER@$HOST: $cmd $@" >> $logfile 2>/dev/null
 
+#
+# NB: using CDATA in the error messages doesn't seem to help with bad characters
+#
+error()
+{
+    echo "<?xml version='1.0'?><error>"
+    while [ "$#" -ge 1 ]; do echo "$1"; shift; done
+    echo "</error>"
+    exit 1
+}
+
+
 # find initial SGE_* parameters
 unset settings
 while [ "$#" -gt 0 ]
 do
     case "$1" in
     SGE_CELL=*)
-        eval "export ${1%%/}"
+        export SGE_CELL="${1##SGE_CELL=}"
         shift
         ;;
     SGE_ROOT=*)
-        eval "export ${1%%/}"
+        export SGE_ROOT="${1##SGE_ROOT=}"
         settings=true
         shift
         ;;
@@ -43,18 +63,36 @@ do
 done
 
 
+# require a good SGE_ROOT and an absolute path:
+[ -d "$SGE_ROOT" -a "${SGE_ROOT##/}" != "$SGE_ROOT" ] || \
+    error "invalid SGE_ROOT directory '$SGE_ROOT'"
+
+# require a good SGE_CELL:
+[ -d "$SGE_ROOT/${SGE_CELL:-default}" ] || \
+    error "invalid SGE_CELL directory '$SGE_ROOT/${SGE_CELL:-default}'"
+
+
+# this is the essential bit from settings.sh,
+# but SGE_ROOT might be different
 if [ ${settings:-false} = true ]
 then
-    # this is the essential bit from settings.sh,
-    # but SGE_ROOT might be different
-    if [ -x $SGE_ROOT/util/arch ]
+    if [ -x "$SGE_ROOT/util/arch" ]
     then
-         PATH=$SGE_ROOT/bin/`$SGE_ROOT/util/arch`:$PATH
-         export PATH
+        PATH=$SGE_ROOT/bin/$($SGE_ROOT/util/arch):$PATH
+        export PATH
+    else
+        error "'$SGE_ROOT/util/arch' not found"
     fi
 fi
 
 
-$cmd "$@"
+case "$cmd" in
+qhost)
+    $cmd "$@" | sed -e 's@xmlns=@xmlns:xsd=@'
+    ;;
+*)
+    $cmd "$@" | sed -e 's@</*>@@g'
+    ;;
+esac
 
 # ----------------------------------------------------------------- end-of-file
